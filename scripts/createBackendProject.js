09d45184b3d93ea058a9a203eac5504c23839b70
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-03-27 15:27:58 +0100

    refactor(backends): folders per technology

--- scripts/createBackendProject.js
+++ scripts/createBackendProject.js
@@ -1,22 +1,22 @@
-import createAPIPlatformProject from './backend/createAPIPlatformProject.js';
-import createExpressProject from './backend/createExpressProject.js';
-import createSpringBootProject from './backend/createSpringBootProject.js';
+import createAPIPlatformProject from "./backend/apiplatform/createAPIPlatformProject.js";
+import createExpressProject from "./backend/express/createExpressProject.js";
+import createSpringBootProject from "./backend/springboot/createSpringBootProject.js";
 
 export default async function ({ technology, projectName }) {
-    console.log(`Creating a backend project using ${technology}...`);
-    console.log(`Project name: ${projectName}`);
+  console.log(`Creating a backend project using ${technology}...`);
+  console.log(`Project name: ${projectName}`);
 
-    switch (technology) {
-        case 'api-platform':
-            await createAPIPlatformProject({ projectName });
-            break;
-        case 'express':
-            await createExpressProject({ projectName });
-            break;
-        case 'spring-boot':
-            await createSpringBootProject({ projectName });
-            break;
-        default:
-            console.error('Unknown technology selected.');
-    }
+  switch (technology) {
+    case "api-platform":
+      await createAPIPlatformProject({ projectName });
+      break;
+    case "express":
+      await createExpressProject({ projectName });
+      break;
+    case "spring-boot":
+      await createSpringBootProject({ projectName });
+      break;
+    default:
+      console.error("Unknown technology selected.");
+  }
 }
