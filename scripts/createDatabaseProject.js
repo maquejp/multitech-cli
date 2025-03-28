dffa3184c6a5eb6b18567d94b20e171888e3585b
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-03-28 15:44:59 +0100

    refactor: :recycle: Folder preparation

--- scripts/createDatabaseProject.js
+++ scripts/createDatabaseProject.js
@@ -1,26 +1,26 @@
-import createOracleProject from './database/createOracleProject.js';
-import createMongoDBProject from './database/createMongoDBProject.js';
-import createMariaDBProject from './database/createMariaDBProject.js';
-import createPostgreSQLProject from './database/createPostgreSQLProject.js';
+import createOracleProject from "./database/createOracleProject.js";
+import createMongoDBProject from "./database/createMongoDBProject.js";
+import createMariaDBProject from "./database/createMariaDBProject.js";
+import createPostgreSQLProject from "./database/postgresql/createPostgreSQLProject.js";
 
 export default async function ({ technology, projectName }) {
-    console.log(`Creating a database project using ${technology}...`);
-    console.log(`Project name: ${projectName}`);
+  console.log(`Creating a database project using ${technology}...`);
+  console.log(`Project name: ${projectName}`);
 
-    switch (technology) {
-        case 'oracle':
-            await createOracleProject({ projectName });
-            break;
-        case 'mongodb':
-            await createMongoDBProject({ projectName });
-            break;
-        case 'mariadb':
-            await createMariaDBProject({ projectName });
-            break;
-        case 'postgresql':
-            await createPostgreSQLProject({ projectName });
-            break;
-        default:
-            console.error('Unknown technology selected.');
-    }
+  switch (technology) {
+    case "oracle":
+      await createOracleProject({ projectName });
+      break;
+    case "mongodb":
+      await createMongoDBProject({ projectName });
+      break;
+    case "mariadb":
+      await createMariaDBProject({ projectName });
+      break;
+    case "postgresql":
+      await createPostgreSQLProject({ projectName });
+      break;
+    default:
+      console.error("Unknown technology selected.");
+  }
 }
