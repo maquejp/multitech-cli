64a66eb46d336498930da0fe75160aedd10237a3
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-04-11 12:11:56 +0200

    feat: a simple hello world

--- cli.js
+++ cli.js
@@ -1,1 +1,11 @@
-console.log("Cool CLI");
+#!/usr/bin/env node
+
+/**
+ * @maquestiaux-devs/multitech-cli
+ * A simple CLI tool for project generation
+ */
+
+console.log('🚀 Welcome to MultiTech CLI!');
+console.log('Version:', process.env.npm_package_version || '0.1.0-alpha.7');
+console.log('\nThis CLI will help you create projects based on your selected category and technology.');
+console.log('More features coming soon! 🎉\n');
