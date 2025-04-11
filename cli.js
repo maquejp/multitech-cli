d9a72bd6ac96eb4f1a77c8935c723e6e9ac80e09
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-04-11 11:49:06 +0200

    fix: bug with the published version

--- cli.js
+++ cli.js
@@ -1,27 +1,13 @@
 import fs from "fs";
-import figlet from "figlet";
-import chalk from "chalk";
 
 async function main() {
-    const packageJson = JSON.parse(
-      fs.readFileSync(new URL("./package.json", import.meta.url))
-    );
-    const version = packageJson.version;
-    const bugReportUrl = packageJson.bugs.url;
-    const author = packageJson.author.name;
-  
-    console.log(
-      chalk.green(
-        figlet.textSync("Project Creator", { horizontalLayout: "full" })
-      )
-    );
-    console.log(
-      chalk.cyan(
-        `This tool will help you create projects based on your selected category and technology.\n`
-      )
-    );
-    console.log(chalk.gray(`Version: ${version} | Author: ${author}\n`));
-    console.log(chalk.yellow(`\nReport a bug: ${bugReportUrl}`));
+  const packageJson = JSON.parse(
+    fs.readFileSync(new URL("./package.json", import.meta.url))
+  );
+  const version = packageJson.version;
+  const bugReportUrl = packageJson.bugs.url;
+  const author = packageJson.author.name;
+  console.log(version, bugReportUrl, author);
 }
 
 main();
