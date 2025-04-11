f94d250b97e045c27b0ba47a9691e8c78e5071ae
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-04-11 14:57:04 +0200

    feat: add interactive menu with create, help, and quit options

--- cli.js
+++ cli.js
@@ -10,6 +10,7 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import chalk from 'chalk';
 import figlet from 'figlet';
+import inquirer from 'inquirer';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -28,3 +29,46 @@ console.log(logo);
 console.log(chalk.green(`Version: ${chalk.bold(packageJson.version)}  | Author: ${chalk.bold(packageJson.author.name)}`));
 console.log(chalk.yellow(`\n${chalk.bold(packageJson.description)}`));
 console.log(chalk.magenta('\nMore features coming soon! 🎉\n'));
+
+// Main menu function
+async function showMainMenu() {
+  const { action } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        { name: '🚀 Create a new project', value: 'create' },
+        { name: '❓ Help', value: 'help' },
+        { name: '👋 Quit', value: 'quit' }
+      ]
+    }
+  ]);
+
+  switch (action) {
+    case 'create':
+      console.log(chalk.cyan('\nCreating a new project...'));
+      // TODO: Implement project creation logic
+      break;
+    case 'help':
+      console.log(chalk.cyan('\nHelp Information:'));
+      console.log(chalk.yellow('This CLI tool helps you create new projects with various technologies.'));
+      console.log(chalk.yellow('Select "Create a new project" to start building your next project!'));
+      break;
+    case 'quit':
+      console.log(chalk.green('\nThank you for using MultiTech CLI! Goodbye! 👋\n'));
+      process.exit(0);
+  }
+
+  // Return to main menu unless quitting
+  if (action !== 'quit') {
+    console.log('\n');
+    await showMainMenu();
+  }
+}
+
+// Start the CLI
+showMainMenu().catch(error => {
+  console.error(chalk.red('An error occurred:'), error);
+  process.exit(1);
+});
